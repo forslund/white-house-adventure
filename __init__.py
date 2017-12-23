80c8ba5e3856461fd5abc9748fe78b2c739f3722
Åke Forslund <ake.forslund@gmail.com>
2017-12-23 21:30:57 +0100

    Add comments.

--- __init__.py
+++ __init__.py
@@ -8,15 +8,22 @@ import sys
 
 
 def clear_until_prompt(zork):
+    """ Clear all received characters until the standard prompt. """
     # Clear all data with title etecetera
     char = zork.stdout.read(1)
     while char != '>':
         char = zork.stdout.read(1)
 
 def cmd(zork, action):
+    """ Write a command to the interpreter. """
     zork.stdin.write(action + '\n')
 
 def zork_read(zork):
+    """
+        Read from zork interpreter process.
+
+        Returns tuple with Room name and description.
+    """
     # read Room name
     output = ""
     output += zork.stdout.read(1)
@@ -47,19 +54,29 @@ class ZorkSkill(MycroftSkill):
 
     @intent_handler(IntentBuilder('PlayZork').require('Play').require('Zork'))
     def play_zork(self, Message):
+        """
+            Starts zork and activates the converse part where the actual game
+            is played.
+        """
         if not self.zork:
+            # Start zork using frotz
             self.zork = subprocess.Popen([self.interpreter, self.data],
                                          stdin=subprocess.PIPE,
                                          stdout=subprocess.PIPE)
-            time.sleep(0.5)
-            clear_until_prompt(self.zork)
+            time.sleep(0.5)  # Allow to load
+            clear_until_prompt(self.zork) # Clear initial startup messages
 
+        # Issue look command to get initial description
         cmd(self.zork, 'look')
         self.room, description = zork_read(self.zork)
         self.speak(description, expect_response=True)
         self.playing = True
 
     def converse(self, utterance, lang):
+        """
+            Pass sentence on to the frotz zork interpreter. The commands
+            "quit" and "exit" will immediately exit the game.
+        """
         utterance = utterance[0]
         if self.playing:
             if "quit" in utterance or utterance == "exit":
@@ -67,6 +84,7 @@ class ZorkSkill(MycroftSkill):
                 self.playing = False
                 return True
             else:
+                # Send utterance to zork interpreter and then speak response
                 cmd(self.zork, utterance)
                 self.room, description = zork_read(self.zork)
                 if description != "":
@@ -75,6 +93,9 @@ class ZorkSkill(MycroftSkill):
         return False
 
     def stop(self, message=None):
+        """
+            Stop playing
+        """
         self.playing = False
 
 
