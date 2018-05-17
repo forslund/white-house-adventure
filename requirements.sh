616689b170a44fdbc1051edf887957197c63802d
Åke Forslund <ake.forslund@gmail.com>
2018-05-17 13:06:23 +0200

    Don't download / install zork if it exists

--- requirements.sh
+++ requirements.sh
@@ -7,9 +7,11 @@ make dumb
 cd ..
 
 #download zork
-mkdir zork
-cd zork
-wget --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" http://www.infocom-if.org/downloads/zork1.zip
-unzip zork1.zip
+if [[ ! -e zork ]]; then
+    mkdir zork
+    cd zork
+    wget --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" http://www.infocom-if.org/downloads/zork1.zip
+    unzip zork1.zip
+fi
 
 cd ${ROOT}
