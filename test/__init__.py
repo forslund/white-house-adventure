9cc27a174d7d92783a5aa8d051c327f997efedf2
Åke Forslund <ake.forslund@gmail.com>
2018-06-05 11:58:14 +0200

    Mock out the subprocess call when testing

--- test/__init__.py
+++ test/__init__.py
@@ -1,7 +1,24 @@
 from unittest.mock import patch, MagicMock
 from test.integrationtests.skills.skill_tester import SkillTest
+import time
 
-def test_runner(skill, example, emitter, loader):
+
+class mock_subprocess():
+    def __init__(self):
+        self.data = b'\n.:>'
+        self.pos = 0
+        self.stdout = self
+        self.stdin = MagicMock()
+
+    def read(self, size=1):
+        ret = self.data[self.pos:self.pos + 1]
+        self.pos = (self.pos + 1) % len(self.data)
+        return ret
+
+
+@patch('subprocess.Popen')
+def test_runner(skill, example, emitter, loader, popen):
+    popen.return_value = mock_subprocess()
     # Get the skill object from the skill path
     s = [s for s in loader.skills if s and s.root_dir == skill][0]
     s.save_file = 'dummy'
